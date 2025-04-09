2f8d1b56b3c19d6ec333fc307a8976d4fdb7e64d
Saadat-B <saadatbadgujar@gmail.com>
2025-04-09 10:47:24 +0530

    interfaces

--- go-basics-tutorial/main.go
+++ go-basics-tutorial/main.go
@@ -1,19 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type User struct {
 	Name     string
 	Age      int
 	IsActive bool
 }
+type Robot struct {
+	Name     string
+	Age      int
+	IsActive bool
+}
+
+type Greet interface {
+	Greeting() string
+}
+
+func (u User) Greeting() string {
+	return "Hello I'm " + u.Name + " aged about " + strconv.Itoa(u.Age) + "years\n"
+}
+func (r Robot) Greeting() string {
+	return "Hello I'm " + r.Name + " aged about " + strconv.Itoa(r.Age) + "years\n"
+}
+
+func printGreet(g Greet) string {
+	greeting := g.Greeting()
+	return greeting
+}
 
 func main() {
 	fmt.Println("structs")
 
-
 	firstUser := User{Name: "firstUser", Age: 5, IsActive: true}
+	firstRobot := Robot{Name: "firstRobot", Age: 10, IsActive: true}
 
-	fmt.Printf("%#v",firstUser)
+	// fmt.Printf("%#v", firstUser)
+	// fmt.Printf("%#v", firstRobot)
+
+	fmt.Print(printGreet(firstUser))
+	fmt.Print(printGreet(firstRobot))
 
 }
