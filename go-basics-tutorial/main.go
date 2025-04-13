3e7b65700e8a28072ef9f8d8121021f84e255a63
Saadat-B <saadatbadgujar@gmail.com>
2025-04-13 13:14:36 +0530

    error handling

--- go-basics-tutorial/main.go
+++ go-basics-tutorial/main.go
@@ -1,47 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strconv"
 )
 
-type User struct {
-	Name     string
-	Age      int
-	IsActive bool
-}
-type Robot struct {
-	Name     string
-	Age      int
-	IsActive bool
-}
-
-type Greet interface {
-	Greeting() string
-}
-
-func (u User) Greeting() string {
-	return "Hello I'm " + u.Name + " aged about " + strconv.Itoa(u.Age) + "years\n"
-}
-func (r Robot) Greeting() string {
-	return "Hello I'm " + r.Name + " aged about " + strconv.Itoa(r.Age) + "years\n"
-}
-
-func printGreet(g Greet) string {
-	greeting := g.Greeting()
-	return greeting
-}
-
 func main() {
-	fmt.Println("structs")
 
-	firstUser := User{Name: "firstUser", Age: 5, IsActive: true}
-	firstRobot := Robot{Name: "firstRobot", Age: 10, IsActive: true}
+	divideNum, err := divide(2, 0)
 
-	// fmt.Printf("%#v", firstUser)
-	// fmt.Printf("%#v", firstRobot)
+	if err != nil {
+		fmt.Println(err)
 
-	fmt.Print(printGreet(firstUser))
-	fmt.Print(printGreet(firstRobot))
+	}
 
+	fmt.Println(divideNum)
+
+}
+
+func divide(a float64, b float64) (float64, error) {
+
+	if b == 0 {
+		return 0, errors.New("divisor cannot be 0")
+	}
+
+	return a / b, nil
 }
