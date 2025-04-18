41e8639fa3967687358e8f28008aecd2c999c92c
Saadat-B <saadatbadgujar@gmail.com>
2025-04-18 12:49:27 +0530

    make POST request

--- go-basics-tutorial/main.go
+++ go-basics-tutorial/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
-const myurl = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+const geturl = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+const posturl = "https://api.freeapi.app/api/v1/users/register"
 
 func main() {
 	fmt.Println("Welcome to web verb video ")
 
-	PerformGetRequest(myurl)
+	// PerformGetRequest(geturl)
+	PerformPostRequest(posturl)
 }
 
 func PerformGetRequest(myurl string) {
@@ -32,4 +37,37 @@ func PerformGetRequest(myurl string) {
 	fmt.Println("status code", response.StatusCode)
 	fmt.Println("content length", response.ContentLength)
 
+}
+
+func PerformPostRequest(myurl string) {
+
+	requestBody := strings.NewReader(`
+
+	{
+ 		"email": "saadatb.email@domain.com",
+  		"password": "test@123",
+  		"role": "ADMIN",
+  		"username": "saadatb"
+	}
+	`)
+
+	response, err := http.Post(posturl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	data, _ := io.ReadAll(response.Body)
+
+	var prettyJSON bytes.Buffer
+
+	err = json.Indent(&prettyJSON, data, "", " ")
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Print(prettyJSON.String())
 }
