050f1ea9d73552aaeb80604479455d348916329e
Saadat-B <saadatbadgujar@gmail.com>
2025-04-19 10:28:54 +0530

    create JSON data from struct

--- go-basics-tutorial/main.go
+++ go-basics-tutorial/main.go
@@ -1,104 +1,37 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"strings"
 )
 
-const geturl = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
-const posturl = "https://api.freeapi.app/api/v1/users/register"
+type course struct {
+	Name     string `json:"name"`
+	Price    int    `json:"price"`
+	Platform string
+	Password string   `json:"-"`
+	Tags     []string `json:"Tags,omitempty"`
+}
 
 func main() {
-	fmt.Println("Welcome to web verb video ")
 
-	// PerformGetRequest(geturl)
-	// PerformPostRequest(posturl)
-	PerformPostFormRequest(posturl)
+	fmt.Println("Welcome to the JSON video")
+	EncodeJson()
 }
 
-func PerformGetRequest(myurl string) {
+func EncodeJson() {
 
-	response, err := http.Get(myurl)
+	lcoCourses := []course{
+		{"React", 299, "web", "abc123", []string{"new"}},
+		{"Angular", 199, "web", "abc123", []string{"old"}},
+		{"Nextjs", 399, "web", "abc123", nil},
+	}
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", " ")
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer response.Body.Close()
-
-	data, _ := io.ReadAll(response.Body)
-
-	content := string(data)
-
-	fmt.Println(content)
-	fmt.Println("status code", response.StatusCode)
-	fmt.Println("content length", response.ContentLength)
-
-}
-
-func PerformPostRequest(myurl string) {
-
-	requestBody := strings.NewReader(`
-
-	{
- 		"email": "saadatb.email@domain.com",
-  		"password": "test@123",
-  		"role": "ADMIN",
-  		"username": "saadatb"
-	}
-	`)
-
-	response, err := http.Post(posturl, "application/json", requestBody)
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer response.Body.Close()
-
-	data, _ := io.ReadAll(response.Body)
-
-	var prettyJSON bytes.Buffer
-
-	err = json.Indent(&prettyJSON, data, "", " ")
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Print(prettyJSON.String())
-}
-
-func PerformPostFormRequest(myurl string) {
-	data := url.Values{}
-
-	data.Add("email", "saadatbad.email@domain.com")
-	data.Add("password", "ADMItestN")
-	data.Add("username", "saadadtba")
-	data.Add("role", "ADMIN")
-
-	response, err := http.PostForm(myurl, data)
-
-	if err != nil {
-		panic(err)
-	}
-	defer response.Body.Close()
-
-	content, _ := io.ReadAll(response.Body)
-
-	var prettyJSON bytes.Buffer
-
-	err = json.Indent(&prettyJSON, content, "", " ")
-
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(prettyJSON.String())
-
+	fmt.Printf("%s\n", finalJson)
 }
